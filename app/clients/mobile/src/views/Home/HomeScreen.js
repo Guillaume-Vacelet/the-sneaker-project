50eea35f2a3dbf725f5f56090a8e4b16aa67a0a4
Mathieu Ney <ney.mathieu@gmail.com>
2021-04-27 14:16:27 +0200

    btn suite

--- app/clients/mobile/src/views/Home/HomeScreen.js
+++ app/clients/mobile/src/views/Home/HomeScreen.js
@@ -18,7 +18,7 @@ export default function HomeScreen(props) {
 
   return (
     <View style={styles.rootContainer}>
-      <Text style={styles.title}>Vérifiez vos <Text style={{fontWeight:'bold', color:}}>sneakers</Text> rapidement !</Text>
+      <Text style={styles.title}>Vérifiez vos <Text style={{fontWeight:'bold'}}>sneakers</Text> rapidement !</Text>
       <SneakerCard />
       <Text style={styles.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       <Button color="#841584" title="Sign out" onPress={handleSignOut} />
