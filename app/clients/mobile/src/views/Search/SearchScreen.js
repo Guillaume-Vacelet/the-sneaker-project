33038f85a53052fd5c46ff72efeb82e6a795fc4f
guillaume-vacelet <guillaume.vacelet@epitech.eu>
2021-04-29 01:05:53 +0200

    :sparkles: Added search bar. There's a bug i need to fix which is causing the bottom of SearchBar to be faded.

--- app/clients/mobile/src/views/Search/SearchScreen.js
+++ app/clients/mobile/src/views/Search/SearchScreen.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { SafeAreaView, View, Text, StatusBar, StyleSheet, Image } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import { SearchTabsNavigator } from '../../core/navigation/SearchTabsNavigator';
 import BrandsContext from '../../core/contexts/BrandsContext';
 import UserProductsContext from '../../core/contexts/UserProductsContext';
+import { FlatGrid } from 'react-native-super-grid';
+import SneakerCard from '../../components/SneakerCard';
+// images
 import nmd_r1 from '../../../assets/sneaker-example.png';
 import nikeLogo from '../../../assets/nike-logo.svg';
 import adidasLogo from '../../../assets/adidas-logo.svg';
 import newbalanceLogo from '../../../assets/new-balance-logo.svg';
 
 export default function SearchScreen() {
+  const [searchInput, setSearchInput] = React.useState('');
   const userProducts = [
     {id: '0', model: 'NMD_R1', brand: 'Adidas', color: 'pink', price: '180.00', image: Image.resolveAssetSource(nmd_r1).uri, checked: true},
     {id: '1', model: 'Air Force 1', brand: 'Nike', color: 'white', price: '100.00', image: Image.resolveAssetSource(nmd_r1).uri, checked: true},
@@ -30,7 +35,27 @@ export default function SearchScreen() {
       <UserProductsContext.Provider value={userProducts}>
         <SafeAreaView style={styles.rootContainer}>
           <Text style={styles.title}>Que recherchez-vous ?</Text>
-            <SearchTabsNavigator userProducts={userProducts} />
+          <SearchBar
+            placeholder="Chercher une paire..."
+            onChangeText={setSearchInput}
+            value={searchInput}
+            clearIcon={true}
+            searchIcon={false}
+            containerStyle={styles.searchBarContainer}
+            inputContainerStyle={styles.searchBarInputContainer}
+          />
+          {searchInput
+            ? <FlatGrid
+                data={userProducts.filter(product => product.model === searchInput)}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                  <SneakerCard item={item} hasBeenChecked={false} />
+                )}
+                itemDimension={150}
+                spacing={10}
+              />
+            : <SearchTabsNavigator userProducts={userProducts} />
+          }
         </SafeAreaView>
       </UserProductsContext.Provider>
     </BrandsContext.Provider>
@@ -39,20 +64,25 @@ export default function SearchScreen() {
 
 const styles = StyleSheet.create({
   rootContainer: {
-    // marginTop: StatusBar.currentHeight,
-    // height: '100%',
-    // width: '100%',
-    // display: 'flex',
-    // justifyContent: 'center',
-    // alignItems: 'center',
     backgroundColor: 'white',
     flex: 1
   },
   title: {
     fontSize: 30,
     fontWeight: "600",
-    padding: 20,
+    margin: 20,
     color: "black",
     alignSelf: 'center'
+  },
+  searchBarContainer: {
+    backgroundColor: 'white', 
+    borderBottomColor: 'transparent', 
+    borderTopColor: 'transparent',
+  },
+  searchBarInputContainer: {
+    backgroundColor: 'white', 
+    borderRadius: 50, 
+    borderWidth: 1, 
+    borderColor: 'black',
   },
 });
