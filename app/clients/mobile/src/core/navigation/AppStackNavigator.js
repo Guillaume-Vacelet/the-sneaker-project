ae1d34f3bf8d278dd6ac006f70f5e4953768ac59
guillaume-vacelet <guillaume.vacelet@epitech.eu>
2021-05-18 11:07:05 +0200

    :fire: Removed not working animation transitions in AppStackNavigator.

--- app/clients/mobile/src/core/navigation/AppStackNavigator.js
+++ app/clients/mobile/src/core/navigation/AppStackNavigator.js
@@ -1,64 +1,36 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from "../../views/Home/HomeScreen";
-// import SettingsScreen from '../../views/Settings/SettingsScreen';
+import SettingsStackNavigator from './SettingsStackNavigator';
 import ProfileScreen from "../../views/Profile/ProfileScreen";
 import ScanScreen from "../../views/Scan/ScanScreen";
-import SettingsStackNavigator from './SettingsStackNavigator';
-
-const config = {
-  animation: 'spring',
-  config: {
-    stiffness: 1000,
-    damping: 500,
-    mass: 3,
-    overshootClamping: true,
-    restDisplacementThreshold: 0.01,
-    restSpeedThreshold: 0.01,
-  },
-};
 
 const AppStack = createStackNavigator();
 const AppStackNavigator = () => (
-  <AppStack.Navigator initialRouteName="Home" 
+  <AppStack.Navigator initialRouteName="Home" mode={"card"}
     screenOptions={{
       gestureEnabled: false,
     }}
   >
-    <AppStack.Screen name="Home" component={HomeScreen} options={{
-      headerShown: false,
-      gestureEnabled: false,
-      transitionSpec: {
-        open: config,
-        close: config
-      }
+    <AppStack.Screen name="Home" component={HomeScreen} 
+      options={{
+        headerShown: false,
       }}
     />
-    <AppStack.Screen name="Settings" component={SettingsStackNavigator} options={{
-      headerShown: false,
-      gestureEnabled: true,
-      transitionSpec: {
-        open: config,
-        close: config
-      }
+    <AppStack.Screen name="Settings" component={SettingsStackNavigator} 
+      options={{
+        headerShown: false,
       }} 
     />
-    <AppStack.Screen name="Profile" component={ProfileScreen} options={{
-      headerShown: false,
-      gestureEnabled: false,
-      gestureDirection: 'horizontal-inverted',
-      transitionSpec: {
-        open: config,
-        close: config
-      }
+    <AppStack.Screen name="Profile" component={ProfileScreen} 
+      options={{
+        headerShown: false,
+        gestureDirection: 'horizontal-inverted',
       }} 
     />
-    <AppStack.Screen name="Scan" component={ScanScreen} options={{
-      headerShown: false,
-      transitionSpec: {
-          open: config,
-          close: config
-        }
+    <AppStack.Screen name="Scan" component={ScanScreen} 
+      options={{
+        headerShown: false,
       }} 
     />
   </AppStack.Navigator>
