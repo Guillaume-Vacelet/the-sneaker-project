00b69eda5b2f656f101d24890e22b751971d251d
guillaume-vacelet <guillaume.vacelet@epitech.eu>
2021-06-24 17:00:15 +0200

    Cleaning without db

--- app/server/flask/app/app.py
+++ app/server/flask/app/app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 # from flask_cors import CORS
-from flask_mail import Mail, Message
-import os
+# from flask_mail import Mail, Message
+# import os
 
 app = Flask(__name__)
 app.secret_key = b'\x9e\xc8\x12\xb9\x1cW\x98\xe4\x17@\xf5\x9e\x86\x0c\x1a\x92'
 # app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'safecheckapp@gmail.com'
-app.config['MAIL_PASSWORD'] = 'BC9qqF7z!fU1'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+# app.config['MAIL_SERVER']='smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'safecheckapp@gmail.com'
+# app.config['MAIL_PASSWORD'] = 'BC9qqF7z!fU1'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
 
 # CORS
 # CORS(app)
@@ -22,25 +22,25 @@ app.config['MAIL_USE_SSL'] = True
 # })
 
 # Mail
-mail = Mail(app)
+# mail = Mail(app)
 
 
 # Database
-from pymongo import MongoClient
+# from pymongo import MongoClient
 # MONGO_HOST = "mongo" 
-MONGO_HOST = "worker.1"
-MONGO_PORT = "27017"
-MONGO_DB = "safecheck-db"
-MONGO_USER = "root"
-MONGO_PASS = "pwd"
-client = MongoClient("mongodb://{}:{}@{}:{}/{}?authSource=admin".format(
-  MONGO_USER, 
-  MONGO_PASS, 
-  MONGO_HOST, 
-  MONGO_PORT, 
-  MONGO_DB
-))
-db = client['safecheck-db']
+# MONGO_HOST = "worker.1"
+# MONGO_PORT = "27017"
+# MONGO_DB = "safecheck-db"
+# MONGO_USER = "root"
+# MONGO_PASS = "pwd"
+# client = MongoClient("mongodb://{}:{}@{}:{}/{}?authSource=admin".format(
+#   MONGO_USER, 
+#   MONGO_PASS, 
+#   MONGO_HOST, 
+#   MONGO_PORT, 
+#   MONGO_DB
+# ))
+# db = client['safecheck-db']
 # Set index to automatically delete user if email_verified==False for 24h
 # db.users.create_index(
 #     "creation_date", 
@@ -49,5 +49,5 @@ db = client['safecheck-db']
 # )
 
 # Routes
-from .user import routes
+# from .user import routes
 from . import views
--- app/server/flask/wsgi.py
+++ app/server/flask/wsgi.py
@@ -1,8 +1,6 @@
 from app.app import app
-import os
+# import os
 
 if __name__ == "__main__":
-  print(os.environ)
-  print(os.environ.keys)
-  port = int(os.environ.get('PORT', '5000'))
-  app.run(host='0.0.0.0', port=port)
+  # port = int(os.environ.get('PORT', '5000'))
+  app.run(host='0.0.0.0', port=5000)
